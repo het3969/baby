50f82889f777cb8116dec0c25899521b07ade6eb
het3969 <gamergop36@gmail.com>
2022-03-07 09:11:19 +0600

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items: 7,
+        loop: true,
+        margin: 13,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+
+    });
+});
